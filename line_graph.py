d2c86ba6ed5ec7abe154796118394adedd7a8761
Blueblueteacher <dngusekgus2@gmail.com>
2022-10-31 10:39:38 +0900

    데이터 시각화 공부 (박스 플롯 그래프)

--- line_graph.py
+++ line_graph.py
@@ -1,4 +1,4 @@
-'''
+''' # 꺾은선그래프
 #%%
 from numpy.random import randn 
 import matplotlib.pyplot as plt
@@ -21,6 +21,7 @@ plt.xlabel('Draw')
 plt.ylabel('Random number')
 plt.legend(loc='best')
 '''
+''' # 막대그래프
 # %%
 from numpy.random import randn 
 import matplotlib.pyplot as plt
@@ -41,4 +42,26 @@ plt.xticks(customers_index, customers, rotation=0, fontsize='small')
 plt.xlabel('Customer name')
 plt.ylabel('Sale amount')
 plt.title('Sale Amount per Customer')
+# %%
+''' # box plot 그래프 => 서로 다른 데이터군을 쉽게 비교할 수 있는 데이터 시각화 방법
+# %%
+import numpy as np
+import matplotlib.pyplot as plt
+plt.style.use('ggplot')
+normal = np.random.normal(loc=0.0, scale=1.0, size=500)
+lognormal = np.random.lognormal(mean=0.0, sigma=1.0, size=500)
+index_value = np.random.randint(0, 500)
+normal_sample= normal[index_value]
+lognormal_sample= lognormal[index_value]
+box_plot_data = [normal, normal_sample, lognormal, lognormal_sample]
+
+fig = plt.figure()
+ax1 = fig.add_subplot(1,1,1)
+box_labels = ['normal', 'normal_sample', 'lognormal', 'lognormal_sample'] # 그래프 라벨
+ax1.boxplot(box_plot_data, notch=False, sym='.', vert=True, whis=1.5, showmeans=True, labels=box_labels)
+ax1.xaxis.set_ticks_position('bottom') 
+ax1.yaxis.set_ticks_position('left')
+ax1.set_title('Box plots: Resampling of two distributions') # 그래프 제목
+plt.xlabel('Distrbution')
+plt.ylabel('Value')
 # %%
