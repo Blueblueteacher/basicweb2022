21ef27f3aed639376308ddf96eb6baf8f02b0718
Blueblueteacher <dngusekgus2@gmail.com>
2022-11-01 11:08:01 +0900

    데이터 시각화 공부 (원그래프, 히스토그램)

--- line_graph.py
+++ line_graph.py
@@ -43,6 +43,7 @@ plt.xlabel('Customer name')
 plt.ylabel('Sale amount')
 plt.title('Sale Amount per Customer')
 # %%
+'''
 ''' # box plot 그래프 => 서로 다른 데이터군을 쉽게 비교할 수 있는 데이터 시각화 방법
 # %%
 import numpy as np
@@ -64,4 +65,37 @@ ax1.yaxis.set_ticks_position('left')
 ax1.set_title('Box plots: Resampling of two distributions') # 그래프 제목
 plt.xlabel('Distrbution')
 plt.ylabel('Value')
+# %%
+'''
+# %% # 원 그래프
+import matplotlib.pyplot as plt
+labels =['Frog', 'Hogs', 'Dogs', 'Logs']
+sizes = [15,30, 45, 10] # labels에 해당하는 값의 양
+explode = (0, 0.3, 0, 0) # 튀어나온정도
+
+fig, ax = plt.subplots()
+ax.pie(sizes, explode=explode, labels= labels, autopct='%1.1f%%',
+	shadow=True, startangle=90)
+ax.axis('equal')
+# %% # 히스토그램 그래프
+import numpy as np
+import matplotlib.pyplot as plt
+plt.style.use('ggplot')
+
+mu1, mu2, sigma = 100, 130, 153
+x1 = mu1+sigma*np.random.randn(10000) # 랜덤값
+x2 = mu2+sigma*np.random.randn(10000) # 랜덤값
+
+fig =plt.figure()
+ax1 = fig.add_subplot(1,1,1)
+n, bins, patches = ax1.hist(x1, bins=50, density=False, color='darkgreen')
+n, bins, patches = ax1.hist(x2, bins=50, density=False, color='orange', alpha=0.5)
+
+ax1.xaxis.set_ticks_position('bottom')
+ax1.yaxis.set_ticks_position('left')
+
+plt.xlabel('Bins') # 아래쪽 이름
+plt.ylabel('Number of values in Bin') # 왼쪽열 이름
+fig.suptitle('Histograms', fontsize=14, fontweight='bold') # 그래프 제목
+ax1.set_title('Two Frequency Distributions') # 그래프 소제목
 # %%
