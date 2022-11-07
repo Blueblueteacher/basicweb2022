2b71b75272f202d47880866d738783184d05cbbe
Blueblueteacher <dngusekgus2@gmail.com>
2022-11-07 10:45:05 +0900

    코드업 1269번, 1273번 풀이

---
+++ [CodeUp] 1269,1273.c
@@ -0,0 +1,33 @@
+/*
+// 1269 수열의 값 구하기
+#include<stdio.h>
+
+int main() 
+{
+    int a, b, c, n; // 시작 값 : a, 곱할 값 : b. 더할 값 : c. 항 : n
+    int answer; // 값을 저장할 변수
+    scanf("%d %d %d %d", &a, &b, &c, &n); 
+    answer = a; // 첫번째 항 저장
+    for (int i = 0; i < n-1; i++) { // 두번째 항부터 ~ n까지
+        answer = answer * b + c;
+    }
+    printf("%d", answer);
+
+    return 0;
+}
+*/
+// 1273 약수 구하기
+#include<stdio.h>
+
+int main() 
+{
+    int n; // 입력받은 수
+    scanf("%d", &n);
+
+    for (int i = 1; i <= n; i++) { // 1부터 n까지 나눠보기
+        if (n % i == 0)
+            printf("%d ", i);
+    }
+
+    return 0;
+}
